0982607df7ba425f14831b202cbacab15b29199d
fahimChowdhury234 <fahimmuntacir234@gmail.com>
2024-05-12 16:50:08 +0600

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,35 +12,6 @@
       }
    }
 
-   function thmOwlInit() {
-      // owl slider
-
-      if ($(".thm-owl__carousel").length) {
-         $(".thm-owl__carousel").each(function () {
-            let elm = $(this);
-            let options = elm.data("owl-options");
-            let thmOwlCarousel = elm.owlCarousel(options);
-         });
-      }
-
-      if ($(".thm-owl__carousel--custom-nav").length) {
-         $(".thm-owl__carousel--custom-nav").each(function () {
-            let elm = $(this);
-            let owlNavPrev = elm.data("owl-nav-prev");
-            let owlNavNext = elm.data("owl-nav-next");
-            $(owlNavPrev).on("click", function (e) {
-               elm.trigger("prev.owl.carousel");
-               e.preventDefault();
-            });
-
-            $(owlNavNext).on("click", function (e) {
-               elm.trigger("next.owl.carousel");
-               e.preventDefault();
-            });
-         });
-      }
-   }
-
    $(window).on("load", function () {
       thmSwiperInit();
    });
